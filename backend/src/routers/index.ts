ebd94c01d5a2d05054d82e4a7c1161055907252b
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2025-08-20 16:37:52 +0200

    feat: create event controller and router

--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 const mainRouter = Router();
 
 import { userRouter } from './user.js';
+import { eventRouter } from './event.js';
 
 // route de test
 mainRouter.get('/', (_req, res) => {
@@ -9,5 +10,6 @@ mainRouter.get('/', (_req, res) => {
 });
 
 mainRouter.use(userRouter);
+mainRouter.use(eventRouter);
 
 export { mainRouter }
