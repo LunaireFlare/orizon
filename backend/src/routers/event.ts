581c5d5aa4bbfa39e9d338562bbcad63c8bb6202
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2025-08-24 14:54:05 +0200

    feat: add auth middleware to all routes

--- backend/src/routers/event.ts
+++ backend/src/routers/event.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { eventController } from '../controllers/event.js';
+import { authMiddleware } from '../middlewares/auth.js';
 const eventRouter = Router();
 
 eventRouter.route('/events')
-    .get(eventController.getAllEvents)
-    .post(eventController.createEvent);
+    .get(authMiddleware, eventController.getAllEvents)
+    .post(authMiddleware, eventController.createEvent);
 
 eventRouter.route('/events/:id')
-    .get(eventController.getOneEvent);
+    .get(authMiddleware, eventController.getOneEvent);
 
 export { eventRouter };
