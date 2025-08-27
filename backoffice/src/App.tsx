e2b5f3b909d2c002703c3504830aae822fdffb10
cheiknac <cheikna.coulibaly@hotmail.fr>
2025-08-27 10:39:25 +0200

    add css page connexion

--- backoffice/src/App.tsx
+++ backoffice/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router';
 import BackOfficeUsersPage from './Page/BackOfficePage/BackOfficeUsersPage.tsx';
 import BackOfficeEventsPage from './Page/BackOfficePage/BackOfficeEventsPage.tsx';
+import BackOfficeConnexion from './Page/BackOfficePage/BackOfficeConnexion.tsx'
 import './App.scss'
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
   return (
         <div>
             <Routes>
-              <Route path="/" element={<BackOfficeUsersPage />} />
+              <Route path="/" element={<BackOfficeConnexion />} />
+              <Route path="/users" element={<BackOfficeUsersPage />} />
               <Route path="/evenements" element={<BackOfficeEventsPage />} />
             </Routes>
         </div>
