a9ce520748db36331edfde2a5d48fb05d9f58f1e
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2025-08-28 13:51:29 +0200

    chore: set up test files

---
+++ backend/tests/func/sum.test.js
@@ -0,0 +1,12 @@
+import { describe, it, expect } from "@jest/globals"
+import { sum } from './sum.js';
+
+describe('Sum function', () =>{
+    it('Returns correct value', () =>{
+        expect(sum(2, 3)).toEqual(5)
+    }),
+
+    it('Should not return incorrect value', () =>{
+        expect(sum(2, 3)).not.toBe(6);
+    })
+})
