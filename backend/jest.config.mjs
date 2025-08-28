46e641d4feb964d2d86f0b19e4fcb854c8d5b8e2
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2025-08-28 13:49:08 +0200

    build: create jest config (ts + js)

---
+++ backend/jest.config.mjs
@@ -0,0 +1,87 @@
+// const config = {
+//   preset: 'ts-jest/presets/default-esm',
+//   testEnvironment: 'node',
+
+//   extensionsToTreatAsEsm: ['.ts'],
+
+//   transform: {
+//     '^.+\\.ts$': ['ts-jest', {
+//       useESM: true,
+//       tsconfig: './tsconfig.json',
+//     }],
+//   },
+
+//   roots: ['<rootDir>/tests'],
+//   testMatch: ['**/*.test.ts'], 
+
+//   moduleNameMapper: {
+//     '^(\\.{1,2}/.*)\\.js$': '$1',
+//   },
+
+//   transform: {
+//     '^.+\\.ts$': ['ts-jest', { useESM: true }],
+//   },
+
+//   transformIgnorePatterns: ['node_modules'],
+
+//   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+// };
+
+// export default config;
+
+
+// const config = {
+//   preset: 'ts-jest/presets/default-esm',
+//   testEnvironment: 'node',
+
+//   extensionsToTreatAsEsm: ['.ts'],
+
+//   transform: {
+//     '^.+\\.ts$': ['ts-jest', {
+//       useESM: true,
+//       tsconfig: './tsconfig.json',
+//     }],
+//   },
+
+//   roots: ['<rootDir>/tests'],
+//   testMatch: ['**/*.test.js'], 
+
+//   moduleNameMapper: {
+//     '^(\\.{1,2}/.*)\\.js$': '$1',
+//   },
+
+//   transform: {
+//     '^.+\\.ts$': ['ts-jest', { useESM: true }],
+//   },
+
+//   transformIgnorePatterns: ['node_modules'],
+
+//   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+// };
+
+// export default config;
+
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+
+  extensionsToTreatAsEsm: ['.ts'],
+
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+      tsconfig: './tsconfig.json',
+    }],
+  },
+
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.js'],
+
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+
+  transformIgnorePatterns: ['node_modules'],
+
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+};
