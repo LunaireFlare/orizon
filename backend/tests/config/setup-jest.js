faf83b729e6f50a0b734655363d4581ee2a90dd8
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2025-10-08 15:52:03 +0200

    chore: delete old tests

--- backend/tests/config/setup-jest.js
+++
@@ -1,9 +0,0 @@
-import { sequelize } from '../database/sequelize/client.js';
-
-beforeEach(async () => {
-    await sequelize.sync({ force: true });
-});
-
-afterAll(async () => {
-    await sequelize.close();
-});
