2a072f98d28b089fa03c711174daa4aef58b0b81
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2025-11-11 10:02:59 +0100

    chore: update max rate limit

--- backend/src/config/rate_limit.ts
+++ backend/src/config/rate_limit.ts
@@ -1,7 +1,7 @@
-import rateLimit from "express-rate-limit";
+import { rateLimit } from "express-rate-limit";
 
 export default rateLimit({
     windowMs: 10 * 60 * 1000,
-    max: 5,
+    max: 20,
     message: "Trop de tentatives. Réessayez plus tard.",
 });
